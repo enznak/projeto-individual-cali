ddf20ed73899c6f2822d2e02c01bc84da1c14d84
enznak <enzo.mazakina@sptech.school>
2026-05-09 16:28:30 -0300

    a

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,16 +1,16 @@
 var usuarioModel = require("../models/usuarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var nome = req.body.nomeServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
 
-        usuarioModel.autenticar(email, senha)
+        usuarioModel.autenticar(nome, senha)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -26,7 +26,7 @@ function autenticar(req, res) {
                             perm: resultadoAutenticar[0].perm
                         });
                     } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
+                        res.status(403).send("Nome de usuário e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }

12ec20780c50555ad0a02fbcb612db51038550d4
enznak <enzo.mazakina@sptech.school>
2026-05-14 18:02:00 -0300

    update: dashboard

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idUsuario, limite_linhas) {
 
     var instrucaoSql = `SELECT 
         dht11_temperatura as temperatura, 
@@ -15,7 +15,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idUsuario) {
 
     var instrucaoSql = `SELECT 
         dht11_temperatura as temperatura, 
